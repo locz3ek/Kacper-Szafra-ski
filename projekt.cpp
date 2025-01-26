91e71be48ce1c5ce779feccb0974af8bbc89b997
locz3ek <kacper.szafranski2004@gmail.com>
2025-01-26 19:03:58 +0100

    Główny

---
+++ projekt.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+void wyswietl_dzien(int x) {
+	switch (x) {
+
+	case 0:
+		cout << "niedziela";
+		break;
+	case 1:
+		cout << "poniedzialek";
+		break;
+	case 2:
+		cout << "wtorek";
+		break;
+	case 3:
+		cout << "sroda";
+		break;
+	case 4:
+		cout << "czwartek";
+		break;
+	case 5:
+		cout << "piatek";
+		break;
+	case 6:
+		cout << "sobota";
+		break;
+
+	}
+}
+
+
+
+
+int main(int argc, char** argv) {
+
+
+
+	if (argc == 2) {
+		int dzien = atoi(argv[1]);
+		wyswietl_dzien(dzien);
+	}
+	else
+	{
+
+		time_t czas;
+		struct tm czas_info;
+
+		time(&czas);
+		localtime_s(&czas_info, &czas);
+
+		int numerDniaTygodnia = czas_info.tm_wday;
+
+		wyswietl_dzien(numerDniaTygodnia);
+	}
+
+
+
+
+
+	return 0;
+}
